a029a02ba69e3cbb559064d603dd261af425b2ce
박범수 <qjatn1102@naver.com>
2026-04-07 16:27:58 +0900

    crud 기능및 게시판 페이징 처리 완료 미완부분 - 예외처리 README.md 작성, 게시판 조회수 기능 추가

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -3,7 +3,6 @@ package com.example.demo.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
