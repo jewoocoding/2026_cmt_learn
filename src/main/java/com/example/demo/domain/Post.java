f4846dfc5445b73abeda2ee2fe07213c69cd12fe
박범수 <qjatn1102@naver.com>
2026-04-07 13:15:31 +0900

    crud 구현밑 웹화면 구현

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -1,16 +1,56 @@
 package com.example.demo.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+@Data
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+//이게 왜 안먹지?
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
 
-    private int idx;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //실간
+    @Column(name = "post_id")
+    private long id;
+    private String title;
+    private String content;
+    private String author;
+    private long viewcount;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
+
+    public String getTitle() {
+
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+
+        return author;
+    }
+
+    public void viewCount() {
+        viewcount++;
 }
+}
+
+
+
+
+
--- src/main/java/com/example/demo/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PostServiceImpl.java
@@ -1,11 +1,64 @@
 package com.example.demo.service.impl;
 
+
+import com.example.demo.domain.Post;
+import com.example.demo.repositories.PostRepository;
 import com.example.demo.service.PostService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
+
+
+    private final PostRepository postRepository;
+
+    @Override
+    public Post save(Post post) {
+        Post postSave = Post.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .author(post.getAuthor())
+                .build();
+
+        return postRepository.save(postSave);
+    }
+
+    @Override
+    public List<Post> findAll() {
+
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findbyId(long id) {
+        return postRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디를 찾을수없다"));
+    }
+
+    // update
+    @Override
+    @Transactional
+    public Post update(long id, Post post) {
+        // 1. 수정할 기존 데이터를 먼저 조회합니다.
+        Post postUpdate = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("아이디를 찾을수없다.: " + id));  //?
+        postUpdate.setTitle(post.getTitle());
+        postUpdate.setContent(post.getContent());
+
+
+        return postUpdate;
+    }
+
+    @Override public void delete(long id) {
+       postRepository.deleteById(id);
+
+   }
 
 }
